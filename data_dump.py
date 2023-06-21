5c17f7b471e26a4dd225796b438e814dc5a5b01f
vinodkumargr <vks7483437@gmail.com>
2023-06-21 19:59:01 +0530

    pushed data into mongo db

---
+++ data_dump.py
@@ -0,0 +1,20 @@
+import pymongo
+import pandas as pd
+import json
+
+client = pymongo.MongoClient("mongodb+srv://vinod:vinod@cluster0.s34epab.mongodb.net/?retryWrites=true&w=majority")
+
+
+DATA_FILE_PATH = "/home/vinod/projects_1/bank_churn_pred/churn.csv"
+DATABASE_NAME = "BANK_CHURN"
+COLLECTION_NAME = "BANK_CHURN"
+
+
+if __name__ == "__main__":
+    data = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns : {data.shape}")
+        
+    json_record = list(json.loads(data.T.to_json()).values())
+    print(json_record[0])
+    
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
